c7cd74e4d5539aec00971a8ef5bb5a66cb967e13
stitchgk <stitchgk@163.com>
2025-06-03 15:18:25 +0800

    feat: 🎸 implement MyClassLoader (#11)
    merge

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,9 +1,50 @@
 package club.shengsheng;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
 
+    protected Class<?> loadClass(String name, boolean resolve)
+            throws ClassNotFoundException
+    {
+        synchronized (getClassLoadingLock(name)) {
+            Class<?> c = findLoadedClass(name);
+            if (c == null) {
+                if (name.startsWith("tech")) {
+                    c = findClass(name);
+                } else {
+                    c = getParent().loadClass(name);
+                }
+            }
+            if (resolve) {
+                resolveClass(c);
+            }
+            return c;
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String path = "加密.class";
+        File classFile = new File(path);
+        try {
+            byte[] bytes = decrypt(Files.readAllBytes(classFile.toPath()));
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name);
+        }
+    }
+
+    private byte[] decrypt(byte[] bytes) {
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) (bytes[i] - 1);
+        }
+        return bytes;
+    }
 }
