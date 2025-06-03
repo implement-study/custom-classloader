ee6586d13aa3a6f5e4835e81427b36dc1847e8c7
会飞的猪猪bb <1257193879@qq.com>
2025-06-03 20:32:20 +0800

    feat: submit homework implementation of custom class loader
    - Implement MyClassLoader to load encrypted .class files
    - Override loadClass to customize class loading logic
    - Decrypt bytes during loading by subtracting 1 from each byte
    - Locate class files based on user.dir system property

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,9 +1,50 @@
 package club.shengsheng;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
+    //
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+
+        synchronized (getClassLoadingLock(name)) {
+            Class<?> loadClass = findLoadedClass(name);
+            if (loadClass != null) {
+                return loadClass;
+            }
+            if (name.startsWith("tech")) {
+                loadClass = findClass(name);
+            } else {
+                loadClass = getParent().loadClass(name);
+            }
+            if (resolve) {
+                resolveClass(loadClass);
+            }
+            return loadClass;
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        try {
+            File file = new File(System.getProperty("user.dir") + File.separatorChar + "加密.class");
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (bytes[i] - 1);
+            }
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name);
+        }
+    }
 
 }
