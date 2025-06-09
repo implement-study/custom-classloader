1ca6c9e2aab79743ab8cafae11612f98d4674532
hangqiii <145259555+jakelerke@users.noreply.github.com>
2025-06-09 20:29:36 +0800

    完成

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,9 +1,33 @@
 package club.shengsheng;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
+
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        String path = "加密.class";
+        File file = new File(".", path);
+        try {
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            for(int i = 0; i < bytes.length; i++) {
+                bytes[i] = --bytes[i];
+            }
+            return defineClass(name, bytes, 0, bytes.length);
+
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name);
+        }
+    }
+
+
+
 
 }
