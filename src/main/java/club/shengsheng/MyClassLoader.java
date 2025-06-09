efebdf7907061d574125268a2b0f04c079b1a8d6
hangqiii <145259555+jakelerke@users.noreply.github.com>
2025-06-09 20:48:16 +0800

    新pr

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -2,32 +2,48 @@ package club.shengsheng;
 
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 
-/**
- * @author gongxuanzhangmelt@gmail.com
- **/
 public class MyClassLoader extends ClassLoader {
-
-
-    @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        String path = "加密.class";
-        File file = new File(".", path);
-        try {
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            for(int i = 0; i < bytes.length; i++) {
-                bytes[i] = --bytes[i];
+        @Override
+        protected Class<?> loadClass(String name, boolean resolve)
+                throws ClassNotFoundException
+        {
+            synchronized (getClassLoadingLock(name)) {
+                // First, check if the class has already been loaded
+                Class<?> c = findLoadedClass(name);
+                if (c == null) {
+                    if (name.startsWith("tech")) {
+                        c = findClass(name);
+                    } else {
+                        c = getParent().loadClass(name);
+                    }
+                }
+                if (resolve) {
+                    resolveClass(c);
+                }
+                return c;
             }
-            return defineClass(name, bytes, 0, bytes.length);
+        }
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            String rootFile = System.getProperty("user.dir").concat(".加密".replace('.', File.separatorChar)).concat(".class");
+            File classFile = new File(rootFile);
+            byte[] classBytes;
+            try {
+                classBytes = Files.readAllBytes( classFile.toPath());
+                for (int i = 0; i < classBytes.length; i++) {
+                    classBytes[i] -= 1;
+                }
 
-        } catch (IOException e) {
-            throw new ClassNotFoundException(name);
+            } catch (Exception e) {
+                throw new ClassNotFoundException();
+            }
+            return  defineClass(name, classBytes, 0, classBytes.length);
+
         }
     }
 
 
 
 
-}
