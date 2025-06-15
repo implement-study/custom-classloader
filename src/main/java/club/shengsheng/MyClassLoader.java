0551703769c44ede989e37083680e3b80c2f3557
zhxu <zheng2580369@gmail.com>
2025-06-15 10:01:19 +0800

    fix

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,9 +1,50 @@
 package club.shengsheng;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
 
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return loadClass(name, false);
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        synchronized (getClassLoadingLock(name)) {
+            Class<?> c = findLoadedClass(name);
+            if (c == null) {
+                if (name.startsWith("tech")) {
+                    c = findClass(name);
+                } else {
+                    c = super.loadClass(name, false);
+                }
+            }
+            if (resolve) {
+                resolveClass(c);
+            }
+            return c;
+        }
+
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        File classFile = new File("加密.class");
+        try {
+            byte[] bytes = Files.readAllBytes(classFile.toPath());
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (bytes[i] - 1);
+            }
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name, e);
+        }
+    }
 }
