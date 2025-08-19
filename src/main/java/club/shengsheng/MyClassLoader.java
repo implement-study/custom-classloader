f2e2f7441c9c66fdb669a560b774cf258e8221fc
Ccg1998 <chuanguangc@gmail.com>
2025-08-19 23:46:06 +0800

    Define ClassLoader By Che

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -3,6 +3,7 @@ package club.shengsheng;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Collections;
 
 
 /**
@@ -13,20 +14,16 @@ public class MyClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        byte[] bytes =null;
-        if ("tech.insight.ShengSheng".equals(name)){
-            File file = new File("./加密.class");
-            try (FileInputStream inputStream = new FileInputStream(file);){
-
-                bytes = inputStream.readAllBytes();
-                for (int i = 0; i < bytes.length; i++) {
-                    bytes[i] = (byte) (bytes[i]-1);
-                }
-                return defineClass(name,bytes,0,bytes.length);
-            } catch (Exception e) {
-                throw new ClassNotFoundException();
+        File file = new File("./加密.class");
+        try (FileInputStream inputStream = new FileInputStream(file);) {
+            byte[] bytes = inputStream.readAllBytes();
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (bytes[i] - 1);
             }
+            return defineClass(name, bytes, 0, bytes.length);
+        } catch (Exception e) {
+            throw new ClassNotFoundException();
         }
-        return super.findClass(name);
+
     }
 }
