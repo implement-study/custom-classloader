594e5a5e4aba55e099d40e7951cbbcdd90289b5a
root <root@hkg2-202412291854153eb61b>
2026-01-12 08:45:49 -0500

    Revert "跑通测试 (#38)"
    This reverts commit 0d970dc5cdda68127764f62d955f8e3d3dda9ad9.

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,49 +1,9 @@
 package club.shengsheng;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
 
-    @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        synchronized (getClassLoadingLock(name)) {
-            Class<?> c = findLoadedClass(name);
-
-            if (c == null) {
-                if (name.startsWith("tech.insight.ShengSheng")){
-                    c = findClass(name);
-
-                }else {
-                    c = getParent().loadClass(name);
-                }
-            }
-            if (resolve) {
-                resolveClass(c);
-            }
-            return c;
-        }
-    }
-
-    @Override
-    protected Class<?> findClass(String name)  {
-//        String path = name.replace(".", "/").concat(".class");
-//        File file = new File("/Users/ysj/IdeaProjects/custom-classloader", path);
-        File file = new File("./加密.class");
-        try {
-
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            for (int i = 0; i < bytes.length; i++) {
-                bytes[i] -=1;
-            }
-            return defineClass(name,bytes,0,bytes.length);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
