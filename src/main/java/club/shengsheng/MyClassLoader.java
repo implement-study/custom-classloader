56acff5368b3af0386a24ad8ea93da3c27bf577e
root <root@hkg2-202412291854153eb61b>
2026-01-18 21:13:38 -0500

    Revert "feat(classloader): 实现自定义类加载器功能 (#39)"
    This reverts commit 49c39b06eaa5e59719f8b8d9bcb058b3d85d8c4c.

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,52 +1,9 @@
 package club.shengsheng;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
 
-
-    @Override
-    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        synchronized (getClassLoadingLock(name)) {
-            Class<?> c = findLoadedClass(name);
-            if (c == null) {
-                if (name.startsWith("tech.insight.ShengSheng")){
-                    c = this.findClass(name);
-                }
-                else {
-                    c = getParent().loadClass(name);
-                }
-            }
-            return c;
-        }
-    }
-
-    @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-
-
-        File classFile = new File("./加密.class");
-
-        try {
-            byte[] bytes = Files.readAllBytes(classFile.toPath());
-            byte[] newBytes = new byte[bytes.length];
-            for (int i = 0; i < bytes.length; i++) {
-                byte aByte = bytes[i];
-                aByte = (byte) (aByte - 1);
-                newBytes[i] = aByte;
-            }
-            return defineClass(name, newBytes, 0, bytes.length);
-        }catch (Exception e) {
-            throw new ClassNotFoundException(name);
-        }
-    }
 }
