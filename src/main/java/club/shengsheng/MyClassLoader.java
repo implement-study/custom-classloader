0f030d3b634ed5c7d538c6fb65bd051dc3dc4645
root <root@hkg2-202412291854153eb61b>
2026-01-28 08:09:13 -0500

    Revert "commit (#40)"
    This reverts commit 447c58f860f016ca874488b8393c047e4944c217.

--- src/main/java/club/shengsheng/MyClassLoader.java
+++ src/main/java/club/shengsheng/MyClassLoader.java
@@ -1,47 +1,9 @@
 package club.shengsheng;
 
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class MyClassLoader extends ClassLoader {
 
-    @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
-        Class<?> c = findLoadedClass(name);
-        if (c == null) {
-            if (name.startsWith("tech.insight.")) {
-                c = findClass(name);
-            }else {
-                c = loadClass(name,false);
-            }
-        }
-        return c;
-    }
-
-    @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        // tech.insight.ShengSheng
-        String path = "加密.class";
-        File file = new File(path);
-        try {
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            decoderBytes(bytes);
-            Files.write(new File("shengsheng.class").toPath(), bytes);
-            return defineClass(name, bytes, 0, bytes.length);
-        } catch (IOException e) {
-            throw new ClassNotFoundException(name);
-        }
-    }
-
-    private void decoderBytes(byte[] bytes) {
-        for (int i = 0; i < bytes.length; i++) {
-            byte b = bytes[i];
-            bytes[i] = (byte) (b-1);
-        }
-    }
 }
